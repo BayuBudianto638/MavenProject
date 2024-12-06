03fdcaafa7bd77a042593c19acbbe6048800fead
Bayu Hyuuga <radamanthys_ok@yahoo.com>
2024-12-06 13:29:24 +0700

    Maven project

--- src/main/java/com/shiawase/App.java
+++ src/main/java/com/shiawase/App.java
@@ -1,13 +1,13 @@
 package com.shiawase;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Customer cust = (Customer) context.getBean("customer");
+        cust.Get();
     }
 }
---
+++ src/main/java/com/shiawase/Customer.java
@@ -0,0 +1,9 @@
+package com.shiawase;
+
+public class Customer
+{
+    public void Get()
+    {
+        System.out.println("Get Data Customer");
+    }
+}
