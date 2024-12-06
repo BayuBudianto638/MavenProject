daa9fdb0acc55beee8f80e625311d139d639bf1f
Bayu Hyuuga <radamanthys_ok@yahoo.com>
2024-12-06 09:48:07 +0700

    Maven project

---
+++ src/main/java/com/shiawase/App.java
@@ -0,0 +1,13 @@
+package com.shiawase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
